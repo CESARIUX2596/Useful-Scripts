2a204dd0bbf16b1ded23308389a9dfa7a25d3484
Cesar Torres <catq2596@gmail.com>
2022-05-11 03:01:19 -0700

    xrand scale from 1080p to 1440p

---
+++ other/Linux-Config/set_monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output HDMI-0 --scale 1.3333x1.3333 # Scale monitor from 1080p to 1440p 
+xrandr --output DP-0 --pos 1440x813 # Move monitor to desired position.
--- other/PopOS-Config/set_monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xrandr --output HDMI-0 --scale 1.3333x1.3333
