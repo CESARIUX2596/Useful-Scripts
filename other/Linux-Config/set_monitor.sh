01542c5ca1300805f45581d33481f609e4f4f842
Cesar Torres <catq2596@gmail.com>
2022-06-12 13:14:21 -0700

    Configuration for 3rd monitor

--- other/Linux-Config/set_monitor.sh
+++ other/Linux-Config/set_monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xrandr --output HDMI-0 --scale 1.3333x1.3333 # Scale monitor from 1080p to 1440p 
-xrandr --output DP-0 --pos 1440x813 # Move monitor to desired position.
+xrandr --output DP-0 --pos 1440x580 # Move monitor 1 to desired position.
+xrandr --output DP-2 --pos 4880x1050 # Move monitor 3 to desired position.
