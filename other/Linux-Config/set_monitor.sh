9b9dcddf642f04a5b684c57852911571cc49eaba
Cesar Torres <catq2596@gmail.com>
2023-01-30 02:28:26 -0800

    Update in monitor script

--- other/Linux-Config/set_monitor.sh
+++ other/Linux-Config/set_monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xrandr --output HDMI-0 --scale 1.3333x1.3333 --rate 60 # Scale monitor from 1080p to 1440p 
+xrandr --output HDMI-0 --scale 1.3333x1.3333 # Scale monitor from 1080p to 1440p 
 xrandr --output DP-0 --pos 1440x580 --rate 144 # Move monitor 1 to desired position.
 xrandr --output DP-2 --pos 4880x1050 --rate 165 # Move monitor 3 to desired position.
